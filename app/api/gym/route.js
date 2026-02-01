cf3786a7025b5a3da162a9e00ba99a12e840a54a
andrewBrownConsulting <andrewBrownConsult@gmail.com>
2026-02-01 15:31:09 +0000

    fix api post call

--- app/api/gym/route.js
+++ app/api/gym/route.js
@@ -2,6 +2,6 @@ import { addCompleteServer } from '@/app/todoServerFuncs'
 import { NextResponse } from 'next/server'
 
 export async function POST() {
-  addCompleteServer("Go to gym", 5)
+  await addCompleteServer("Go to gym", 5)
   return NextResponse.json({ message: 'Added gym to today' })
 }
