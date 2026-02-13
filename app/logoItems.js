31e717e6e52f96469af641a959c7adc830250911
andrewBrownConsulting <andrewBrownConsult@gmail.com>
2026-02-13 06:56:32 +0000

    simplify quick items and add to complete rather than todo

--- app/logoItems.js
+++ app/logoItems.js
@@ -1,22 +1,16 @@
 'use client'
 import { Grid, Flex, GridItem, Button, Box, Heading, Select, Portal, createListCollection, Input, Stack } from "@chakra-ui/react"
-import { addTodoServer, } from "./todoServerFuncs";
+import { addCompleteServer, addTodoServer, } from "./todoServerFuncs";
 import { useState } from "react";
 
 const allOptions = [
-  { value: "10 mins", score: 1, info: "Read for 10 mins" },
-  { value: "30 mins", score: 3, info: "Read for 30 mins" },
-  { value: "60 mins", score: 6, info: "Read for 60 mins" },
   { value: "NYTimes", score: 2, info: "Complete NYTimes Games" },
-  { value: "Work 9 to 5", score: 10 },
-  { value: "1 Hour Personal Project", score: 2 },
-  { value: "Write a blog article", score: 5 },
   { value: "Go on Date", score: 10 },
   { value: "Go out with Friends ", score: 10 },
 ]
 export default function LogoItems({ updateLists }) {
   async function addLogoTodo(name, score, info) {
-    addTodoServer(name, score, info);
+    addCompleteServer(name, score, info)
     updateLists();
   }
   const logoItem = (name, options) => (
