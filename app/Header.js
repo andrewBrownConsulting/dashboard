18cdb73271ea47267069d0719c899d581a1c22c5
andrewBrownConsulting <andrewBrownConsult@gmail.com>
2026-08-04 05:33:32 +0100

    update header to use a list rather than hard code

--- app/Header.js
+++ app/Header.js
@@ -1,15 +1,21 @@
 import { Grid, GridItem, Flex, Heading, Text } from "@chakra-ui/react"
 import Link from "next/link"
+const links = [
+  { name: "Andrew Todo", href: '/' },
+  { name: "View All", href: '/all' },
+  { name: "Logs", href: '/logs' },
+  { name: "Books", href: '/books' },
+  { name: "Values", href: '/values' },
+  { name: "Buy", href: '/buy' },
+]
 export default function Header() {
   return (
     <>
       <Grid justify={'center'} gap={10} align="center" gridTemplateColumns={{ 'base': '1fr', 'sm': '1fr 1fr', 'md': '1fr 1fr 1fr', 'lg': 'repeat(6, 1fr)' }}>
-        <GridItem><Link href={'/'}><Heading size={'2xl'} textAlign={'center'}>Andrew Todo</Heading></Link></GridItem>
-        <GridItem><Link href={'/all'} ><Heading textAlign={'center'} size={'2xl'}>View All</Heading></Link></GridItem>
-        <GridItem><Link href={'/logs'}><Heading textAlign={'center'} size={'2xl'}>Logs</Heading></Link></GridItem>
-        <GridItem><Link href={'/books'}><Heading textAlign={'center'} size={'2xl'}>Books</Heading></Link></GridItem>
-        <GridItem><Link href={'/values'}><Heading textAlign={'center'} size={'2xl'}>Values</Heading></Link></GridItem>
-        <GridItem><Link href={'/buy'}><Heading textAlign={'center'} size={'2xl'}>Buy</Heading></Link></GridItem>
+        {links.map((val, key) => {
+          return <GridItem key={key}><Link href={val.href}><Heading size={'2xl'} textAlign={'center'}>{val.name}</Heading></Link></GridItem>
+        })}
+        {/* <GridItem><Link href={'/'}><Heading size={'2xl'} textAlign={'center'}>Andrew Todo</Heading></Link></GridItem> */}
       </Grid>
     </>
   )
